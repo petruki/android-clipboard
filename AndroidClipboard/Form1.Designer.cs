06a091bd3e95ca037f0162af417e95477bb63fb5
petruki <31597636+petruki@users.noreply.github.com>
2020-10-05 12:29:43 -0700

    Added docs

--- AndroidClipboard/Form1.Designer.cs
+++ AndroidClipboard/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AndroidClipboard
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.textArea = new System.Windows.Forms.RichTextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
@@ -62,7 +63,7 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -70,8 +71,11 @@
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.textArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Android Clipboard v0.0.1 - By Roger Floriano";
             this.ResumeLayout(false);
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+Android Clipboard is a simple clipboard app that transfer the content selected from the app to your running ADB device.
+
+# Setup/Howto
+1. Add ADB program to your environment variables.
+	adb.exe is located at your Android SDK installation folder, e.g. D:\Android\Sdk\platform-tools
+2. Make sure you are running your app on the emulator.
